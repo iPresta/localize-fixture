0c16126bc249dcf50f5b1483388a76c53c25435e
mahdi <ramtin2025@yahoo.com>
2017-08-05 05:42:01 -0700

    Add .rtlfix support
    Developers can place a file with .rtlfix extension for override some styles. this file will be append to generated RTL CSS file.

--- src/RTLGenerator.php
+++ src/RTLGenerator.php
@@ -8,27 +8,31 @@
  *
  */
 use \CSSJanus;
-class rtlGenerator {
+class RTLGenerator {
     public static $definitions = array(
         'type' => 'css' // OR 'scss'
     );
     public static function generate($directory){
-        $all_files = Tools::scandir($directory, self::$definitions['type'],'',True);
+        $all_files = @Tools::scandir($directory, self::$definitions['type'],'',True);
         foreach ($all_files as $file)
-            if(substr(rtrim($file,'.'.self::$definitions['type']), -5) != '_rtl'){
+            if(substr(rtrim($file,'.'.self::$definitions['type']), -4) != '_rtl'){
                 $file_content = file_get_contents($directory.'/'.$file);
                 self::make_rtl($file_content, $directory.'/'.$file);
             }
     }
     public static function make_rtl($content, $file) {
         //make rtl current file
-        $css_base = $content;
-        $rtl_content = CSSJanus::transform($css_base);
+        $rtl_content = CSSJanus::transform($content);
         $path = pathinfo($file);
         $rtl_file = $path['dirname'].'/'.$path['filename'].'_rtl.css';
         if (file_exists($rtl_file))
             unlink($rtl_file);
         file_put_contents($rtl_file, $rtl_content);
+        //append rtlfix
+        $rtlfix = $path['dirname'].'/'.$path['filename'].'.rtlfix';
+        if (file_exists($rtlfix)){
+            file_put_contents($rtl_file, file_get_contents($rtlfix) . PHP_EOL, FILE_APPEND);
+        }
         @chmod($rtl_file, 0644);
     }
 }
